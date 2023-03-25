8d466374eb45fbfc9df1191af2362b4795478f16
黄永辉 <huangyh__123@163.com>
2023-03-26 00:54:41 +0100

    add Recommand component and apply the components to the Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <header class="home-header wrap" :class="{ active: state.headerScroll }">
-      <img class="logo" src="../assets/book.svg">
+    <header class="home-header wrap" :class="{ active: this.headerScroll }">
+      <img class="logo" src="../assets/book.svg" />
       <div class="header-search">
         <span class="app-name">BookHub</span>
         <i class="iconfont icon-search"></i>
@@ -9,71 +9,162 @@
           >Search</router-link
         >
       </div>
-      <router-link class="login" tag="span" to="#" v-if="!state.isLogin"
+      <router-link class="login" tag="span" to="#" v-if="!isLogin"
         >Login</router-link
       >
       <router-link class="login" tag="span" to="#" v-else>
         <van-icon name="manager-o" />
       </router-link>
     </header>
-    <swiper :list="state.swiperList"></swiper>
+    <swiper :list="swiperList"></swiper>
   </div>
-  <div style="height: 500px;"></div>
+  <recommand title="Recent Searched" :books="bookList"></recommand>
+  <recommand title="Best selling" :books="bookList"></recommand>
+  <book-list :books="bookList"></book-list>
+  <div style="height: 500px"></div>
 </template>
 
-<script setup>
-import { reactive, onMounted, nextTick } from "vue";
-import { useRouter } from "vue-router";
-import swiper from '@/components/Swiper.vue'
-import { showLoadingToast, closeToast, showToast } from "vant";
-const router = useRouter();
-const state = reactive({
-  swiperList: [], // Carousel list
-  isLogin: false,
-  headerScroll: false, // 滚动透明判断
-  hots: [],
-  newGoodses: [],
-  recommends: [],
-  loading: true,
-});
-onMounted(async () => {
-  // const token = getLocal('token')
-  // if (token) {
-  //   state.isLogin = true
-  //   // get cart data.
-  //   cart.updateCart()
-  // }
-  showLoadingToast({
-    message: "Loading...",
-    forbidClick: true,
-  });
-  state.swiperList = [
-    {id: 1, imageUrl: "./src/static/1.jpeg", url: "#"},
-    {id: 2, imageUrl: "./src/static/2.jpeg", url: "#"},
-    {id: 3, imageUrl: "./src/static/3.jpeg", url: "#"}
-  ]
-  // const { data } = await getHome()
-  // state.swiperList = data.carousels
-  // state.newGoodses = data.newGoodses
-  // state.hots = data.hotGoodses
-  // state.recommends = data.recommendGoodses
-  state.loading = false
-  closeToast();
-});
+<script>
+// import { useRouter } from "vue-router";
+import swiper from "@/components/Swiper.vue";
+// import { showLoadingToast, closeToast, showToast } from "vant";
+import BookList from "@/components/BookList.vue";
+// const router = useRouter();
+import Recommand from "../components/Recommond.vue";
+export default {
+  components: {
+    BookList,
+    Recommand
+},
+  data() {
+    return {
+      swiperList: [
+        { id: 1, imageUrl: "./src/static/1.jpeg", url: "#" },
+        { id: 2, imageUrl: "./src/static/2.jpeg", url: "#" },
+        { id: 3, imageUrl: "./src/static/3.jpeg", url: "#" },
+      ], // Carousel list
+      isLogin: false,
+      headerScroll: false, // 滚动透明判断
+      hots: [],
+      newGoodses: [],
+      recommends: [],
+      loading: true,
+      bookList: [
+  {
+    id: 1,
+    name: 'JavaScript高级程序设计',
+    author: 'Nicholas C. Zakas',
+    type: '计算机科学',
+    price: '89.00',
+    image: "./src/static/1.jpeg"
+  },
+  {
+    id: 2,
+    name: '图解HTTP',
+    author: '上野宣',
+    type: '计算机科学',
+    price: '49.00',
+    image:"./src/static/1.jpeg"
+  },
+  {
+    id: 3,
+    name: '算法（第4版）',
+    author: 'Robert Sedgewick / Kevin Wayne',
+    type: '计算机科学',
+    price: '98.00',
+    image: "./src/static/1.jpeg"
+  },
+  {
+    id: 4,
+    name: '计算机组成与设计 硬件/软件接口',
+    author: 'David A. Patterson / John L. Hennessy',
+    type: '计算机科学',
+    price: '99.00',
+    image:"./src/static/1.jpeg"
+  },
+  {
+    id: 5,
+    name: '代码整洁之道',
+    author: 'Robert C. Martin',
+    type: '计算机科学',
+    price: '59.00',
+    image:"./src/static/1.jpeg"
+  },
+  {
+    id: 6,
+    name: '代码整洁之道',
+    author: 'Robert C. Martin',
+    type: '计算机科学',
+    price: '59.00',
+    image:"./src/static/1.jpeg"
+  },
+  {
+    id: 7,
+    name: '代码整洁之道',
+    author: 'Robert C. Martin',
+    type: '计算机科学',
+    price: '59.00',
+    image:"./src/static/1.jpeg"
+  },
+  {
+    id: 8,
+    name: '代码整洁之道',
+    author: 'Robert C. Martin',
+    type: '计算机科学',
+    price: '59.00',
+    image:"./src/static/1.jpeg"
+  },
+  {
+    id: 9,
+    name: '代码整洁之道',
+    author: 'Robert C. Martin',
+    type: '计算机科学',
+    price: '59.00',
+    image:"./src/static/1.jpeg"
+  },
+  {
+    id: 10,
+    name: '代码整洁之道',
+    author: 'Robert C. Martin',
+    type: '计算机科学',
+    price: '59.00',
+    image:"./src/static/1.jpeg"
+  }
+]
 
-nextTick(() => {
-  document.body.addEventListener("scroll", () => {
-    let scrollTop =
-      window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop;
-    scrollTop > 100
-      ? (state.headerScroll = true)
-      : (state.headerScroll = false);
+    };
+  },
+  mounted() {
+    document.body.addEventListener("scroll", () => {
+      let scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+      scrollTop > 100 ? (this.headerScroll = true) : (this.headerScroll = false);
       const logo = document.querySelector(".logo");
-    state.headerScroll? logo.src = "src/assets/book-active.svg" : logo.src = "src/assets/book.svg";
-  });
-});
+      this.headerScroll
+        ? (logo.src = "src/assets/book-active.svg")
+        : (logo.src = "src/assets/book.svg");
+    });
+    // const token = getLocal('token')
+    // if (token) {
+    //   state.isLogin = true
+    //   // get cart data.
+    //   cart.updateCart()
+    // }
+    // showLoadingToast({
+    //   message: "Loading...",
+    //   forbidClick: true,
+    // });
+    // const { data } = await getHome()
+    // state.swiperList = data.carousels
+    // state.newGoodses = data.newGoodses
+    // state.hots = data.hotGoodses
+    // state.recommends = data.recommendGoodses
+    // loading = false;
+    // closeToast();
+  },
+};
 
 // const goToDetail = (item) => {
 //   router.push({ path: `/product/${item.goodsId}` })
@@ -105,9 +196,10 @@ nextTick(() => {
   }
   &.active {
     background: @primary;
-    
+
     .login {
       color: #fff;
+      margin-right: 20px;
     }
   }
 
@@ -144,93 +236,12 @@ nextTick(() => {
   .login {
     color: @primary;
     line-height: 52px;
+    margin-right: 20px;
     .van-icon-manager-o {
       font-size: 20px;
       vertical-align: -3px;
     }
   }
 }
-.good {
-  .good-header {
-    background: #f9f9f9;
-    height: 50px;
-    line-height: 50px;
-    text-align: center;
-    color: @primary;
-    font-size: 16px;
-    font-weight: 500;
-  }
-  .good-box {
-    display: flex;
-    justify-content: flex-start;
-    flex-wrap: wrap;
-    .good-item {
-      box-sizing: border-box;
-      width: 50%;
-      border-bottom: 1px solid #e9e9e9;
-      padding: 10px 10px;
-      img {
-        display: block;
-        width: 120px;
-        margin: 0 auto;
-      }
-      .good-desc {
-        text-align: center;
-        font-size: 14px;
-        padding: 10px 0;
-        .title {
-          color: #222333;
-        }
-        .price {
-          color: @primary;
-        }
-      }
-      &:nth-child(2n + 1) {
-        border-right: 1px solid #e9e9e9;
-      }
-    }
-  }
-}
-.floor-list {
-  width: 100%;
-  padding-bottom: 50px;
-  .floor-head {
-    width: 100%;
-    height: 40px;
-    background: #f6f6f6;
-  }
-  .floor-content {
-    display: flex;
-    flex-shrink: 0;
-    flex-wrap: wrap;
-    width: 100%;
-    .boxSizing();
-    .floor-category {
-      width: 50%;
-      padding: 10px;
-      border-right: 1px solid #dcdcdc;
-      border-bottom: 1px solid #dcdcdc;
-      .boxSizing();
-      &:nth-child(2n) {
-        border-right: none;
-      }
-      p {
-        font-size: 17px;
-        color: #333;
-        &:nth-child(2) {
-          padding: 5px 0;
-          font-size: 13px;
-          color: @primary;
-        }
-      }
-      .floor-products {
-        .fj();
-        width: 100%;
-        img {
-          .wh(65px, 65px);
-        }
-      }
-    }
-  }
-}
+
 </style>
