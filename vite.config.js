449f02fea3e00c8c0114e87d164582373089dc80
黄永辉 <huangyh__123@163.com>
2023-03-27 22:48:34 +0200

    add base to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bookhub/',
   server: {
     port: 8080
   },
